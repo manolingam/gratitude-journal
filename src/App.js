f89ca1c084eaaec5f10bec35817ecc311ecd7b63
Manolingam <manolingam1996@gmail.com>
2020-01-08 19:45:04 +0530

    added basic UI

--- src/App.js
+++ src/App.js
@@ -1,28 +1,76 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+const useStyles = makeStyles(theme => ({
+	root: {
+		'& > *': {
+			margin: theme.spacing(1),
+			maxWidth: 500
+		}
+	}
+}));
+
+function App() {
+	const classes = useStyles();
+
+	const [journal, setJournal] = useState([]);
+
+	const [text, setText] = useState('');
+
+	const handleSubmit = e => {
+		e.preventDefault();
+		let tempJournal = journal;
+		if (text) {
+			tempJournal.push(text);
+			setJournal(tempJournal);
+			setText('');
+		}
+	};
+
+	return (
+		<div className='App'>
+			{console.log('rendered')}
+			<p id='heading'>Omni Gratitude Journal</p>
+			<p id='sub-title'>Give Gratitude! Get a Token!</p>
+			<form
+				className={classes.root}
+				noValidate
+				autoComplete='off'
+				onSubmit={handleSubmit}
+			>
+				<input
+					className='input'
+					placeholder='What are you grateful for today?'
+					value={text}
+					onChange={e => setText(e.target.value)}
+				/>
+			</form>
+			<div className='journal-container'>
+				{journal.map((item, index) => {
+					return (
+						<Paper
+							key={index}
+							className={classes.root}
+							style={{ marginBottom: '10px' }}
+						>
+							<Typography component='p'>{item}</Typography>
+						</Paper>
+					);
+				})}
+			</div>
+			<input className='email' placeholder='Email' />
+			<div style={{ margin: '10px' }}>
+				<Button variant='contained' style={{ margin: '10px' }}>
+					Register
+				</Button>
+				<Button variant='contained'>Send</Button>
+			</div>
+		</div>
+	);
 }
 
 export default App;
